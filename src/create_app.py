012507f41056c21d8dc429fef4ff5c5a13ea61e1
Mykhailo Popudniak <dev.miquella@gmail.com>
2025-09-25 01:25:13 +0300

    feat(app): додати lifespan з утилізацією підключень до бази даних
    - додано lifespan для керування життєвим циклом застосунку
    - забезпечено коректне закриття engine і пулів з’єднань

--- src/create_app.py
+++ src/create_app.py
@@ -1,4 +1,17 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
 from fastapi import FastAPI
+
+from src.core import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
 
 
 def create_app() -> FastAPI:
