0a413f8652e531942a3b242f76772911a6660f8c
Mykhailo Popudniak <dev.miquella@gmail.com>
2025-09-24 20:41:25 +0300

    feat(app): додати редірект з кореня на /docs

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(
+        url="/docs",
+        status_code=status.HTTP_307_TEMPORARY_REDIRECT,
+    )
