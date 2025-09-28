c5a961fa38c8660977da81b6480746009b53c6a2
JuliMik <gusachenkojulia@gmail.com>
2025-09-28 15:41:46 +0100

    Update photo.py
    Виправила зауваження

--- src/core/models/photo.py
+++ src/core/models/photo.py
@@ -1,4 +1,6 @@
-import uuid as uuid_pkg
+import uuid
+from datetime import datetime
+from typing import Annotated
 
 from sqlalchemy import DateTime, String, Text, func
 from sqlalchemy.dialects.postgresql import UUID
@@ -6,19 +8,21 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 
+Timestamp = Annotated[
+    datetime,
+    mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    ),
+]
+
 
 class Photo(Base):
     __tablename__ = "photos"
-    uuid: Mapped[uuid_pkg.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid_pkg.uuid4
-    )
 
+    uuid: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
     cloudinary_url: Mapped[str] = mapped_column(Text, nullable=False)
-
-    description: Mapped[str] = mapped_column(String(255), nullable=False)
-
-    created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
-
-    updated_at: Mapped[DateTime] = mapped_column(
-        DateTime, server_default=func.now(), onupdate=func.now()
-    )
+    description: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    created_at: Timestamp
+    updated_at: Timestamp
