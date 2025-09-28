5e5bcaa692934559e98485e27736a5e9e586bc8e
JuliMik <gusachenkojulia@gmail.com>
2025-09-28 12:53:33 +0100

    Update photo.py
    Creare model for photo

--- src/core/models/photo.py
+++ src/core/models/photo.py
@@ -1,7 +1,8 @@
-from sqlalchemy import String, Text, DateTime, func
+import uuid as uuid_pkg
+
+from sqlalchemy import DateTime, String, Text, func
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
-import uuid as uuid_pkg
 
 from .base import Base
 
@@ -9,28 +10,15 @@ from .base import Base
 class Photo(Base):
     __tablename__ = "photos"
     uuid: Mapped[uuid_pkg.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        primary_key=True,
-        default=uuid_pkg.uuid4
+        UUID(as_uuid=True), primary_key=True, default=uuid_pkg.uuid4
     )
 
-    cloudinary_url: Mapped[str] = mapped_column(
-        Text,
-        nullable=False
-    )
+    cloudinary_url: Mapped[str] = mapped_column(Text, nullable=False)
 
-    description: Mapped[str] = mapped_column(
-        String(255),
-        nullable=False
-    )
+    description: Mapped[str] = mapped_column(String(255), nullable=False)
 
-    created_at: Mapped[DateTime] = mapped_column(
-        DateTime,
-        server_default=func.now()
-    )
+    created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
 
     updated_at: Mapped[DateTime] = mapped_column(
-        DateTime,
-        server_default=func.now(),
-        onupdate=func.now()
+        DateTime, server_default=func.now(), onupdate=func.now()
     )
