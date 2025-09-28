a1a085b16a1597dca9082735f50872141587a8bf
Mykhailo Popudniak <dev.miquella@gmail.com>
2025-09-28 21:12:56 +0300

    feat(deps): додати анотації для параметрів offset та limit

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,8 +1,11 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core import db_helper
+
+offset_param = Annotated[int, Query(ge=0)]
+limit_param = Annotated[int, Query(gt=0, le=20)]
 
 db_dependency = Annotated[AsyncSession, Depends(db_helper.session_getter)]
