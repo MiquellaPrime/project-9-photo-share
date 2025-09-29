649a096f85c894eaca8770f47067e55ef8685cd0
Mykhailo Popudniak <dev.miquella@gmail.com>
2025-09-30 00:04:07 +0300

    feat(routes): додати ендпоінт для логіну

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter, HTTPException, status
+from typing import Annotated
 
-from src.dependencies import db_dependency
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from src.core.models import UserOrm
+from src.dependencies import db_dependency, token_service_dependency
 from src.repository import users as users_crud
-from src.schemas import UserCreateDto, UserDto
+from src.schemas import TokenInfo, UserCreateDto, UserDto
 from src.services import PasswordHashService
+from src.services import auth as auth_service
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -27,8 +31,21 @@ async def signup(
             session=session,
             body=user_create,
         )
-
     raise HTTPException(
         status_code=status.HTTP_409_CONFLICT,
         detail="User already exists",
+    )
+
+
+@router.post("/login", response_model=TokenInfo)
+async def login(
+    user: Annotated[UserOrm, Depends(auth_service.authenticate_user)],
+    token_service: token_service_dependency,
+):
+    access_token = token_service.create_access_token(user=user)
+    refresh_token = token_service.create_refresh_token(user=user)
+
+    return TokenInfo(
+        access_token=access_token,
+        refresh_token=refresh_token,
     )
