264b4f050904375453709450806f35ebf90ada18
vylia99 <110261644+vylia99@users.noreply.github.com>
2025-09-29 15:03:58 +0200

    feat(security): додати функції хешування та верифікації паролю (#9)
    * feat: add PasswordContext class
    
    * feat: implement PasswordContext
    
    * changed class configurations
    
    * added import
    
    * fixed: type annotation and class initialization
    
    * editing the init function
    
    * fix: fixed pyproject file

---
+++ src/core/pwd_context.py
@@ -0,0 +1,17 @@
+from passlib.context import CryptContext
+
+
+class PasswordContext:
+    """Class for hashing and validating passwords"""
+
+    def __init__(self, schemes: list[str], deprecated: str = "auto"):
+        self._ctx = CryptContext(schemes=schemes, deprecated=deprecated)
+
+    def hash_password(self, password: str) -> str:
+        return self._ctx.hash(password)
+
+    def verify_password(self, password: str, hashed: str) -> bool:
+        return self._ctx.verify(password, hashed)
+
+
+pwd_context = PasswordContext(schemes=["bcrypt"])
