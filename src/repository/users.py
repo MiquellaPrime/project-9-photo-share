3afb391887b21b406b1b7d7ed648862affeca8a9
Mykhailo Popudniak <dev.miquella@gmail.com>
2025-09-29 19:15:08 +0300

    feat(repo): додати repository-layer для роботи з моделю користувача
    - створено функцію для створення користувача
    - створено функцію для отримання користувача за email

---
+++ src/repository/users.py
@@ -0,0 +1,31 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.models import UserOrm
+from src.schemas import UserCreateDto, UserRoles
+
+
+async def create_user(
+    session: AsyncSession,
+    body: UserCreateDto,
+) -> UserOrm:
+    """Create a new user."""
+    user = UserOrm(
+        email=body.email,
+        hashed_password=body.password,
+        role=UserRoles.USER,
+    )
+    session.add(user)
+    await session.commit()
+    return user
+
+
+async def get_user_by_email(
+    session: AsyncSession,
+    email: str,
+) -> UserOrm | None:
+    """Get a user by email."""
+    stmt = select(UserOrm).filter_by(email=email)
+
+    result = await session.execute(stmt)
+    return result.scalars().first()
