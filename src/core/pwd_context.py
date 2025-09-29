654b59f191cdbb7da9ba58fbe9040329c1f38c52
vylia99 <yvasilishina@gmail.com>
2025-09-29 09:38:45 +0200

    fixed: type annotation and class initialization

--- src/core/pwd_context.py
+++ src/core/pwd_context.py
@@ -4,8 +4,8 @@ from passlib.context import CryptContext
 class PasswordContext:
     """Class for hashing and validating passwords"""
 
-    def __init__(self, schemes=None, deprecated="auto"):
-        self._ctx = CryptContext(schemes, deprecated)
+    def __init__(self, schemes: list[str], deprecated: str):
+        self._ctx = CryptContext(schemes=schemes, deprecated=deprecated)
 
     def hash_password(self, password: str) -> str:
         return self._ctx.hash(password)
@@ -14,4 +14,4 @@ class PasswordContext:
         return self._ctx.verify(password, hashed)
 
 
-pwd_context = PasswordContext()
+pwd_context = PasswordContext(schemes=["bcrypt"])
