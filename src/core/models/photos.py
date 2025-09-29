23986359b2aea557903497f298c11d4eec09e27c
JuliMik <gusachenkojulia@gmail.com>
2025-09-29 22:23:21 +0100

    feat: створити клас-міксін

--- src/core/models/photos.py
+++ src/core/models/photos.py
@@ -3,14 +3,13 @@ from uuid import UUID, uuid4
 from sqlalchemy import Text
 from sqlalchemy.orm import Mapped, mapped_column
 
-from .base import UTC_NOW_SQL, Base, str_255, timestamp_tz
+from .base import Base, str_255
+from .mixins import TimeMixin
 
 
-class PhotoOrm(Base):
+class PhotoOrm(Base, TimeMixin):
     __tablename__ = "photos"
 
     uuid: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
     cloudinary_url: Mapped[str] = mapped_column(Text, nullable=False)
     description: Mapped[str_255 | None]
-    created_at: Mapped[timestamp_tz]
-    updated_at: Mapped[timestamp_tz] = mapped_column(onupdate=UTC_NOW_SQL)
