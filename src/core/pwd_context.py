0279f210524be10cbe2a81c3bd1e9d72ce62cca7
vylia99 <yvasilishina@gmail.com>
2025-09-30 10:21:36 +0200

    chore: changed the library to pwdlib[argon2]

--- src/core/pwd_context.py
+++ src/core/pwd_context.py
@@ -1,11 +1,11 @@
-from passlib.context import CryptContext
+from pwdlib import PasswordHash
 
 
 class PasswordContext:
     """Class for hashing and validating passwords"""
 
-    def __init__(self, schemes: list[str], deprecated: str = "auto"):
-        self._ctx = CryptContext(schemes=schemes, deprecated=deprecated)
+    def __init__(self, scheme: str = "argon2"):
+        self._ctx = PasswordHash(scheme)
 
     def hash_password(self, password: str) -> str:
         return self._ctx.hash(password)
@@ -14,4 +14,4 @@ class PasswordContext:
         return self._ctx.verify(password, hashed)
 
 
-pwd_context = PasswordContext(schemes=["bcrypt"])
+pwd_context = PasswordContext()
