5ec2965d6eb5bf4605e06c1c4a58ac6f7652a608
RuslanaPashkutska <ryslana.pashkytska@gmail.com>
2025-09-30 14:07:17 +0200

    feat(repository): додати репозиторій для PhotoOrm (#18)
    * feat(repository): add repository for PhotoOrm
    
    * feat(repository): delete annotations

---
+++ src/repository/photos/photos.py
@@ -0,0 +1,35 @@
+from uuid import UUID
+
+from sqlalchemy import desc, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.models import PhotoOrm
+from src.schemas.photos import PhotoCreateDTO
+
+
+async def create_photo(session: AsyncSession, body: PhotoCreateDTO) -> PhotoOrm:
+    photo = PhotoOrm(
+        uuid=body.uuid,
+        cloudinary_url=body.cloudinary_url,
+        description=body.description,
+    )
+    session.add(photo)
+    await session.flush()
+    return photo
+
+
+async def get_photos(
+    session: AsyncSession, offset: int = 0, limit: int = 10
+) -> list[PhotoOrm]:
+    stmt = (
+        select(PhotoOrm).order_by(desc(PhotoOrm.created_at)).offset(offset).limit(limit)
+    )
+
+    result = await session.execute(stmt)
+    return result.scalars().all()
+
+
+async def get_photo_by_uuid(session: AsyncSession, photo_uuid: UUID) -> PhotoOrm | None:
+    stmt = select(PhotoOrm).where(PhotoOrm.uuid == photo_uuid)
+    result = await session.execute(stmt)
+    return result.scalar_one_or_none()
