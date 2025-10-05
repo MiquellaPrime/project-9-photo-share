9bde9c7bbb3a326e5b3027e7b5ab446fa22bf8e1
Mykhailo Popudniak <dev.miquella@gmail.com>
2025-10-05 19:21:21 +0300

    feat(deps): додати DI залежність для отримання користувача

--- src/dependencies.py
+++ src/dependencies.py
@@ -3,8 +3,10 @@ from typing import Annotated
 from fastapi import Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.constants import ACCESS_TOKEN_TYPE
 from src.core import db_helper
-from src.services import TokenService
+from src.core.models import UserOrm
+from src.services import TokenService, auth_service
 
 offset_param = Annotated[int, Query(ge=0)]
 limit_param = Annotated[int, Query(gt=0, le=20)]
@@ -12,3 +14,8 @@ limit_param = Annotated[int, Query(gt=0, le=20)]
 db_dependency = Annotated[AsyncSession, Depends(db_helper.session_getter)]
 
 token_service_dependency = Annotated[TokenService, Depends(TokenService)]
+
+user_dependency = Annotated[
+    UserOrm,
+    Depends(auth_service.get_current_user(from_token_type=ACCESS_TOKEN_TYPE)),
+]
