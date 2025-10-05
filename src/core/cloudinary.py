cdd61391283e9d5bf70b1cf493d31d7cee434c4a
Mykhailo Popudniak <dev.miquella@gmail.com>
2025-10-05 19:34:10 +0300

    feat(cloudinary): зберігати світлини для кожного користувача в окремій папці

--- src/core/cloudinary.py
+++ src/core/cloudinary.py
@@ -29,11 +29,16 @@ class CloudinaryClient:
         )
         self.asset_folder = asset_folder
 
-    async def upload_image(self, photo_uuid: UUID, file: BinaryIO) -> UploadImageResult:
+    async def upload_image(
+        self,
+        photo_uuid: UUID,
+        file: BinaryIO,
+        user_id: int,
+    ) -> UploadImageResult:
         """Upload image under public_id=photo_uuid and return upload result."""
         options = {
             "public_id": str(photo_uuid),
-            "asset_folder": self.asset_folder,
+            "asset_folder": f"{self.asset_folder}/user_{user_id}",
             "use_asset_folder_as_public_id_prefix": False,
             "unique_filename": False,
             "resource_type": "image",
